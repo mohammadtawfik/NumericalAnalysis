ba446404a368558d702a196b9247a07ff51286c3
Mohammad Tawfik <mohammad.tawfik@gmail.com>
2020-01-04 08:45:27 +0200

    Add files via upload

---
+++ NumericalIVP01.m
@@ -0,0 +1,21 @@
+clear all
+close all
+clc
+
+Weffect=1; Cc=1; Mass=1;
+
+TMax=4; DeltaT=0.2; Nt=TMax/DeltaT;
+
+VofT(1)=0;
+TVect(1)=0;
+VEuler(1)=VofT(1);
+for ii=2:Nt+1;
+  Tt=(ii-1)*DeltaT;
+  TVect(ii)=Tt;
+  VofT(ii)=Weffect/Cc*(1-exp(-Tt*Cc/Mass));
+  VEuler(ii)=VEuler(ii-1)+DeltaT*((Weffect-Cc*VEuler(ii-1))/Mass);
+end
+
+plot(TVect,VofT,TVect,VEuler)
+grid
+axis([0,4,0,1])
